cdd8e1d5e5ad8c315528d4ce5fa69c5650ec7bc2
NitheeshKumar-C <cnitheeshkumar20@gmail.com>
2022-07-07 11:23:04 +0530

    c1: first commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# Frontend Mentor - Article preview component solution
+
+This is a solution to the [Article preview component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/article-preview-component-dYBN_pYFT). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Design](#design)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the component depending on their device's screen size
+- See the social media share links when they click the share icon
+
+### Design
+
+![](design/desktop-preview.jpg)
+
+### Links
+
+- [Solution here](https://your-solution-url.com)
+- [Live Site here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+
+
+### Useful resources
+
+- [W3Schools-css_transform](https://www.w3schools.com/cssref/playdemo.asp?filename=playcss_transform&preval=none)
+
+
+## Author
+
+- Frontend Mentor - [@NitheeshKumar](https://www.frontendmentor.io/profile/NitheeshKumar-C)
---
+++ js/main.js
@@ -0,0 +1,47 @@
+var shareIcon = document.getElementsByClassName('share-svg-div');
+var shareIcn = document.getElementsByClassName('share-item');
+var shareItems = document.getElementById('share-items-div');
+var shareSvg = document.getElementsByClassName('share-svg');
+var shareit = document.getElementsByClassName('share-items');
+document.addEventListener('click', (evt) => {
+	if (
+		(evt.target == shareIcon[0] || evt.target == shareSvg[0]) &&
+		window.getComputedStyle(shareit[0]).getPropertyValue('display') == 'none'
+	) {
+		shareIcon[0].style.backgroundColor = 'hsl(217, 19%, 35%)';
+		shareit[0].style.display = 'block';
+		setTimeout(() => {
+			shareIcn[0].classList.remove('share-item-anime');
+			shareIcn[1].classList.remove('share-item-anime');
+			shareIcn[2].classList.remove('share-item-anime');
+		}, 1000);
+	} else {
+		var result = false;
+		var shareItemsChilds = shareItems.childNodes;
+		for (let i = 0; i < shareItemsChilds.length; i++) {
+			if (evt.target == shareItemsChilds[i]) {
+				result = true;
+			}
+		}
+		if (!result && evt.target != shareItems) {
+			shareit[0].style.display = 'none';
+			shareIcon[0].style.backgroundColor = 'var(--DesaturatedDarkBlue)';
+			shareIcn[0].classList.add('share-item-anime');
+			shareIcn[1].classList.add('share-item-anime');
+			shareIcn[2].classList.add('share-item-anime');
+		}
+	}
+});
+
+// var codedBy = 'NitheeshKumar';
+// var codedByElement=document.getElementById('codedBy');
+// var i=0
+// typeLetter();
+// function typeLetter() {
+//     if (i < codedBy.length) {
+//       codedByElement.innerHTML += codedBy.charAt(i);
+//       i++;
+//       setTimeout(typeLetter, 90);
+//     }
+//   }
+
